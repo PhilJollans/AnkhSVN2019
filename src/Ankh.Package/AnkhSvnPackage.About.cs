9a251258fe8b8aa53d2041bc1aa288561a1b01f9
PhilJollans <phil@jollans.com>
2019-12-14 01:12:52 +0100

    vbscript file to patch version numbers and build project.
    Add the script file Generate_AnkhSVN2019.vbs.
    Minor change to vsixmanifest so that patching the version works.
    Fix the parameters to InstalledProductRegistration for the Visual Studio about box.
    Patched versions of files with version 1.00,0001

--- src/Ankh.Package/AnkhSvnPackage.About.cs
+++ src/Ankh.Package/AnkhSvnPackage.About.cs
@@ -31,7 +31,7 @@ namespace Ankh.VSPackage
 {
     // This attribute is used to register the informations needed to show the this package
     // in the Help/About dialog of Visual Studio.
-    [InstalledProductRegistration(true, null, null, null)]
+    [InstalledProductRegistration("#110", "#100", "1.00.0.0001", IconResourceID = 400)]
     [Ankh.VSPackage.Attributes.ProvideUIVersion]
     public partial class AnkhSvnPackage : IVsInstalledProduct
     {
--- src/Ankh.VS.UnitTest/Properties/AssemblyInfo.cs
+++ src/Ankh.VS.UnitTest/Properties/AssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.00.0.0001")]
