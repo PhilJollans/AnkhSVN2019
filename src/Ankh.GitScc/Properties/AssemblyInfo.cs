469d071496dd76c084dbee724677cd386193ef8a
PhilJollans <phil@jollans.com>
2019-12-14 09:09:31 +0100

    Minor cleanup of assembly.cs files
    My build script Generate_AnkhSVN2019.vbs it too stupid to update the AssemblyVersion correctly, if it is preceded by a commented out version.
    Similarly, it is too stupid to handle AssemblyVersionAttribute.
    Rather than improving the script, I have cleaned up the assemblyInfo files.

--- src/Ankh.GitScc/Properties/AssemblyInfo.cs
+++ src/Ankh.GitScc/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Resources;
 using Ankh;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: CLSCompliant(true)]
@@ -19,19 +19,18 @@ using Ankh;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.00.0.0001")]
-[assembly: AssemblyVersion("2.7.*")]
+[assembly: AssemblyVersion("1.00.0.0001")]
